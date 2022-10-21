45a72f1be09889edf1946728fca0706d0a27029d
kiriakide <kiriakigew20@gmail.com>
2022-10-21 18:22:47 +0300

    Testing

--- TESTdatacamp.py
+++ TESTdatacamp.py
@@ -1,6 +1,16 @@
-import math
+a = "is"
+b = "nice"
+my_list = ["my", "list", a, b]
 
-A = 16
-print (math.sqrt(A))
+print(my_list)
+x = ["a", "b", "c", "d"]
+print(x[:2])
+print(x[2:])
+print(x[:])
+print()
 
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 
+# Correct the bathroom area
+areas[-1]= 10.50
+print(areas)
---
+++ main.py
@@ -0,0 +1,4 @@
+import math
+
+A = 16
+print (math.sqrt(A))
